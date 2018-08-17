a3547e3d72747464ea73f624df77c58ccf5d92cf
Oliver Thomas <olliethomas86@gmail.com>
2018-08-17 11:12:58 +0000

    Resolve "add TPR Diff metric"

--- ethicml/tests/metric_test.py
+++ ethicml/tests/metric_test.py
@@ -9,6 +9,8 @@ from ethicml.algorithms.svm import SVM
 from ethicml.metrics.accuracy import Accuracy
 from ethicml.metrics.metric import Metric
 from ethicml.evaluators.per_sensitive_attribute import metric_per_sensitive_attribute
+from ethicml.evaluators.per_sensitive_attribute import diff_per_sensitive_attribute
+from ethicml.metrics.tpr import TPR
 from ethicml.tests.run_algorithm_test import get_train_test
 
 
@@ -28,3 +30,13 @@ def test_accuracy_per_sens_attr():
     predictions: np.array = model.run(train, test)
     acc_per_sens = metric_per_sensitive_attribute(predictions, test, Accuracy())
     assert acc_per_sens == {0: 0.8780487804878049, 1: 0.882051282051282}
+
+
+def test_tpr_diff():
+    train, test = get_train_test()
+    model: Algorithm = SVM()
+    predictions: np.array = model.run(train, test)
+    tprs = metric_per_sensitive_attribute(predictions, test, TPR())
+    assert tprs == {0: 0.7704918032786885, 1: 0.9312977099236641}
+    tpr_diff = diff_per_sensitive_attribute(tprs)
+    assert tpr_diff["0-1"] == 0.16080590664497563
