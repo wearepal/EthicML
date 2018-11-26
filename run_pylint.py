bd37785e26ad43fb14203150e5430356346a9603
Oliver Thomas <ot44@sussex.ac.uk>
2018-11-26 22:35:12 +0000

    added thresholding script

---
+++ run_pylint.py
@@ -0,0 +1,15 @@
+"""
+To stop pylint breaking the build if <100% perfect
+"""
+
+import sys
+from pylint.lint import Run
+
+RESULTS = Run(['../ethicml/'], exit=False)
+SCORE = RESULTS.linter.stats['global_note']
+print("Your code has been rated at {:.2f}/10".format(SCORE))
+
+if SCORE >= 8.5:
+    sys.exit(0)
+else:
+    sys.exit(1)
