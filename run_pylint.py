26b6dcb16ef091c671f33c55f9116aa43b91a631
Oliver Thomas <ot44@sussex.ac.uk>
2018-11-26 21:27:05 +0000

    changed flag from do_exit to exit. Assume it's a pylint version thing

--- run_pylint.py
+++ run_pylint.py
@@ -28,7 +28,7 @@ args, remaining_args = parser.parse_known_args()
 
 threshold = args.threshold
 
-run = lint.Run(remaining_args, do_exit=False)
+run = lint.Run(remaining_args, exit=False)
 score = run.linter.stats['global_note'] # Yes this is a terrible name for the score
 print("Your code has been rated at {} / 10.".format(score))
 
