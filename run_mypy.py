fe5786200c6e5108e07b16f7587700efffc137ea
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-02-20 15:40:55 +0000

    Only fail on more than 5 errors

---
+++ run_mypy.py
@@ -0,0 +1,17 @@
+"""Run mypy and report how many errors there are"""
+
+import sys
+from mypy import api as mypy
+
+MAX_ALLOWED_ERRORS = 5
+
+RESULTS = mypy.run(['./ethicml/'])
+print(RESULTS[0], end='')
+print(RESULTS[1], end='')
+NUM_ERRORS = len(RESULTS[0].split('\n')) - 1
+print("There are {} errors. {} are allowed.".format(NUM_ERRORS, MAX_ALLOWED_ERRORS))
+
+if NUM_ERRORS <= MAX_ALLOWED_ERRORS:
+    sys.exit(0)
+else:
+    sys.exit(1)
