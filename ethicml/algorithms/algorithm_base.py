0d8a2b30979e7638922239000aa7f872f40c8a95
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-02-25 14:21:58 +0000

    Improve error handling of threaded algorithms

--- ethicml/algorithms/algorithm_base.py
+++ ethicml/algorithms/algorithm_base.py
@@ -1,8 +1,10 @@
 """
 Base class for Algorithms
 """
-
+import sys
 from abc import ABC, abstractmethod
+from typing import List, Optional, Dict
+from subprocess import check_call, CalledProcessError
 
 
 class Algorithm(ABC):
@@ -16,8 +18,37 @@ class Algorithm(ABC):
 
 class ThreadedAlgorithm(ABC):
     """Base class for algorithms that run in their own thread"""
+    def __init__(self, executable: Optional[str] = None):
+        """Constructor
+
+        Args:
+            executable: (optional) path to a (Python) executable. If not provided, the Python
+                        executable that called this script is used.
+        """
+        if executable is None:
+            # use the python executable that this script was called with
+            executable = sys.executable
+        self.executable: str = executable
+
     @property
     @abstractmethod
     def name(self) -> str:
         """Name of the algorithm"""
         raise NotImplementedError()
+
+    def _call_script(self, script: str, args: List[str], env: Optional[Dict[str, str]] = None):
+        """This function calls a (Python) script as a separate process
+
+        An exception is thrown if the called script failed.
+
+        Args:
+            script: path to the (Python) script
+            args: list of strings that are passed as commandline arguments to the script
+            env: environment variables specified as a dictionary; e.g. {"PATH": "/usr/bin"}
+        """
+        cmd = [self.executable, script]
+        cmd += args
+        try:
+            check_call(cmd, env=env)
+        except CalledProcessError:
+            raise RuntimeError(f'The script "{script}" failed. Supplied arguments: {args}')
--- ethicml/algorithms/inprocess/threaded/logistic_regression.py
+++ ethicml/algorithms/inprocess/threaded/logistic_regression.py
@@ -1,8 +1,4 @@
 """Threaded logistic regression"""
-import sys
-from subprocess import call
-from typing import Optional
-
 import numpy as np
 import pandas as pd
 
@@ -11,27 +7,19 @@ from .threaded_in_algorithm import ThreadedInAlgorithm
 
 class ThreadedLR(ThreadedInAlgorithm):
     """Threaded logistic regression"""
-    def __init__(self, python_exe: Optional[str] = None):
-        if python_exe is None:
-            # use the python executable that this script was called with
-            python_exe = sys.executable
-        self.python_exe = python_exe
-
     def run(self, train_paths, test_paths, tmp_path):
         # path where the predictions are supposed to be stored
         pred_path = tmp_path / "predictions.npy"
 
+        # the following strings are passed to the other script
         args = [
-            self.python_exe,
-            "examples/logistic_regression.py",
-            # the following strings are passed to the other script
             str(train_paths.x),
             str(train_paths.y),
             str(test_paths.x),
             str(pred_path)
         ]
         # call the script (this is blocking)
-        call(args)
+        self._call_script("examples/logistic_regression.py", args)
         # load the results
         predictions = np.load(pred_path)
         return pd.DataFrame(predictions, columns=["preds"])
