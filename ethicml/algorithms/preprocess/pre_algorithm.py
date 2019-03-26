55237679c1507c878c072a11ecac3b8f61ff9334
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-03-26 18:13:55 +0000

    Make _load_output non-private

--- ethicml/algorithms/preprocess/pre_algorithm.py
+++ ethicml/algorithms/preprocess/pre_algorithm.py
@@ -41,7 +41,7 @@ class PreAlgorithm(Algorithm):
             tmp_path = Path(tmpdir)
             train_paths, test_paths = self.write_data(train, test, tmp_path)
             train_path, test_path = self.run_thread(train_paths, test_paths, tmp_path)
-            return self._load_output(train_path), self._load_output(test_path)
+            return self.load_output(train_path), self.load_output(test_path)
 
     def run_thread(self, train_paths: PathTuple, test_paths: PathTuple, tmp_path: Path) -> (
             Tuple[pd.DataFrame, pd.DataFrame]):
