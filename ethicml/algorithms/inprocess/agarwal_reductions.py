911ad29e8c2be86a88a28b8e387c3238527e8623
Oliver Thomas <ot44@sussex.ac.uk>
2019-03-27 14:37:02 +0000

    Added Agarwal method

---
+++ ethicml/algorithms/inprocess/agarwal_reductions.py
@@ -0,0 +1,53 @@
+"""
+implementation of Agarwal model
+"""
+from pathlib import Path
+from typing import List
+
+import pandas as pd
+
+from ethicml.algorithms.inprocess.in_algorithm import InAlgorithm
+from ethicml.algorithms.utils import PathTuple, DataTuple
+from ethicml.implementations import agarwal
+
+
+VALID_FAIRNESS = {"DP", "EqOd"}
+VALID_MODELS = {"LR", "SVM"}
+
+
+class Agarwal(InAlgorithm):
+    """
+    Agarwal class
+    """
+    def __init__(self,
+                 fairness: str = "DP",
+                 classifier: str = "LR",
+                 eps: float = 0.1,
+                 iters=50
+                 ):
+        if fairness not in VALID_FAIRNESS:
+            raise ValueError("results: fairness must be one of %r." % VALID_FAIRNESS)
+        if classifier not in VALID_MODELS:
+            raise ValueError("results: classifier must be one of %r." % VALID_MODELS)
+        super().__init__()
+        self.classifier = classifier
+        self.fairness = fairness
+        self.eps = eps
+        self.iters = iters
+
+    def _run(self, train: DataTuple, test: DataTuple) -> pd.DataFrame:
+        return agarwal.train_and_predict(train=train, test=test,
+                                         classifier=self.classifier, fairness=self.fairness,
+                                         eps=self.eps, iters=self.iters)
+
+    def _script_command(self, train_paths: PathTuple,
+                        test_paths: PathTuple, pred_path: Path) -> (List[str]):
+        script = ['-m', agarwal.train_and_predict.__module__]
+        args = self._conventional_interface(train_paths, test_paths, pred_path,
+                                            str(self.classifier), str(self.fairness),
+                                            str(self.eps), str(self.iters))
+        return script + args
+
+    @property
+    def name(self) -> str:
+        return "Agarwal"
---
+++ ethicml/implementations/agarwal.py
@@ -0,0 +1,52 @@
+"""Implementation of logistic regression (actually just a wrapper around sklearn)"""
+import pandas as pd
+
+from fairlearn.classred import expgrad
+from fairlearn.moments import Moment, DP, EO
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+
+from ethicml.algorithms.utils import DataTuple
+from ethicml.utility.heaviside import Heaviside
+from .common import InAlgoInterface
+
+
+def train_and_predict(train: DataTuple, test: DataTuple,
+                      classifier: str, fairness: str,
+                      eps: float, iters: int):
+    """Train a logistic regression model and compute predictions on the given test data"""
+
+    fairness_class: Moment = DP() if fairness == "DP" else EO()
+    if classifier == "SVM":
+        model = SVC(gamma='auto', random_state=888)
+    else:
+        model = LogisticRegression(solver='liblinear', random_state=888)
+
+    data_x = train.x
+    data_y = train.y[train.y.columns[0]]
+    data_a = train.s[train.s.columns[0]]
+
+    res_tuple = expgrad(dataX=data_x, dataA=data_a, dataY=data_y,
+                        learner=model, cons=fairness_class, eps=eps, T=iters)
+
+    res = res_tuple._asdict()
+
+    preds = pd.DataFrame(res['best_classifier'](test.x), columns=["preds"])
+    helper = Heaviside()
+    preds = preds.apply(helper.apply)
+    preds = preds.replace(0, -1)
+    return preds
+
+
+def main():
+    """This function runs the Agarwal model as a standalone program"""
+    interface = InAlgoInterface()
+    train, test = interface.load_data()
+    classifier, fairness, eps, iters, = interface.remaining_args()
+    interface.save_predictions(train_and_predict(train, test,
+                                                 classifier=classifier, fairness=fairness,
+                                                 eps=float(eps), iters=int(iters)))
+
+
+if __name__ == "__main__":
+    main()
