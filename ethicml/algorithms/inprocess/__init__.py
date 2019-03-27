ae64322747e6bfeb6b6b6a66a40d731023df6c6a
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-03-27 16:48:28 +0000

    Add the fair GP models

--- ethicml/algorithms/inprocess/__init__.py
+++ ethicml/algorithms/inprocess/__init__.py
@@ -1,7 +1,7 @@
 """In-process algorithms take training data and make predictions"""
 from .agarwal_reductions import Agarwal
 from .in_algorithm import InAlgorithm
-from .fair_gpyt import GPyT
+from .fair_gpyt import GPyT, GPyTDemPar, GPyTEqOdds
 from .kamishima import Kamishima
 from .logistic_regression import LR
 from .logistic_regression_cross_validated import LRCV
