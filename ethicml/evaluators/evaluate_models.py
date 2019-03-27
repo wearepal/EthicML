3605ff0b1c37ad44b62f22551480043065fb0fd0
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-03-27 12:36:04 +0000

    Remove evaluate_threaded_models

--- ethicml/evaluators/evaluate_models.py
+++ ethicml/evaluators/evaluate_models.py
@@ -1,19 +1,15 @@
 """
 Runs given metrics on given algorithms for given datasets
 """
-import os
 from pathlib import Path
-from tempfile import TemporaryDirectory
-from typing import List, Dict, Tuple, Any, Union
+from typing import List, Dict, Union
 
 import pandas as pd
-import numpy as np
 
-from ethicml.algorithms.algorithm_base import Algorithm, load_dataframe
 from ethicml.algorithms.inprocess.in_algorithm import InAlgorithm
 from ethicml.algorithms.postprocess.post_algorithm import PostAlgorithm
 from ethicml.algorithms.preprocess.pre_algorithm import PreAlgorithm
-from ethicml.algorithms.utils import DataTuple, PathTuple, write_as_feather, get_subset
+from ethicml.algorithms.utils import DataTuple, get_subset
 from ..data.dataset import Dataset
 from ..data.load import load_data
 from .per_sensitive_attribute import (metric_per_sensitive_attribute, MetricNotApplicable,
@@ -23,15 +19,7 @@ from ..preprocessing.train_test_split import train_test_split
 
 
 def get_sensitive_combinations(metrics: List[Metric], train: DataTuple) -> List[str]:
-    """
-
-    Args:
-        metrics:
-        train:
-
-    Returns:
-
-    """
+    """Get all possible combinations of sensitive attribute and metrics"""
     poss_values = []
     for col in train.s:
         uniques = train.s[col].unique()
@@ -42,11 +30,7 @@ def get_sensitive_combinations(metrics: List[Metric], train: DataTuple) -> List[
 
 
 def per_sens_metrics_check(per_sens_metrics: List[Metric]):
-    """
-
-    Args:
-        per_sens_metrics:
-    """
+    """Check if the given metrics allow application per sensitive attribute"""
     for metric in per_sens_metrics:
         if not metric.apply_per_sensitive:
             raise MetricNotApplicable(f"Metric {metric.name} is not applicable per sensitive "
@@ -82,81 +66,7 @@ def evaluate_models(datasets: List[Dataset], preprocess_models: List[PreAlgorith
                     inprocess_models: List[InAlgorithm], postprocess_models: List[PostAlgorithm],
                     metrics: List[Metric], per_sens_metrics: List[Metric],
                     test_mode: bool = False) -> None:
-    """
-
-    Args:
-        datasets:
-        preprocess_models:
-        inprocess_models:
-        postprocess_models:
-        metrics:
-        per_sens_metrics:
-        test_mode:
-    """
-    per_sens_metrics_check(per_sens_metrics)
-
-    for dataset in datasets:
-
-        data: DataTuple = load_data(dataset)
-
-        train_test: Tuple[DataTuple, DataTuple] = train_test_split(data)
-        train, test = train_test
-
-        to_operate_on = {"no_transform": {'train': train,
-                                          'test': test}}
-
-        for pre_process_method in preprocess_models:
-            if test_mode:
-                new_train, new_test = pre_process_method.run_test(train, test)
-            else:
-                new_train, new_test = pre_process_method.run(train, test)
-            to_operate_on[pre_process_method.name] = {
-                'train': DataTuple(x=new_train, s=train.s, y=train.y),
-                'test': DataTuple(x=new_test, s=test.s, y=test.y),
-            }
-
-        columns = ['model']
-        columns += [metric.name for metric in metrics]
-        columns += get_sensitive_combinations(per_sens_metrics, train)
-
-        for name, transform in to_operate_on.items():
-            results = pd.DataFrame(columns=columns)
-
-            transformed_train: DataTuple = transform['train']
-            transformed_test: DataTuple = transform['test']
-            transform_name: str = name
-
-            for model in inprocess_models:
-
-                temp_res: Dict[str, Union[str, float]] = {'model': model.name}
-
-                predictions: np.array
-                if test_mode:
-                    predictions = model.run_test(transformed_train, transformed_test)
-                else:
-                    predictions = model.run(transformed_train, transformed_test)
-
-                temp_res.update(run_metrics(predictions, test, metrics, per_sens_metrics))
-
-                for postprocess in postprocess_models:
-                    # Post-processing has yet to be defined
-                    # - leaving blank until we have an implementation to work with
-                    pass
-
-                results = results.append(temp_res, ignore_index=True)
-            outdir = '../results'
-            if not os.path.exists(outdir):
-                os.mkdir(outdir)
-            results.to_csv(f"../results/{dataset.name}_{transform_name}.csv", index=False)
-
-
-def evaluate_threaded_models(datasets: List[Dataset], preprocess_models: List[PreAlgorithm],
-                             inprocess_models: List[InAlgorithm],
-                             postprocess_models: List[PostAlgorithm], metrics: List[Metric],
-                             per_sens_metrics: List[Metric], test_mode: bool = False):
     """Evaluate all the given models for all the given datasets and compute all the given metrics
-
-    This function only works with threaded models.
 
     Args:
         datasets: list of dataset objects
@@ -174,68 +84,44 @@ def evaluate_threaded_models(datasets: List[Dataset], preprocess_models: List[Pr
         test: DataTuple
         train, test = train_test_split(load_data(dataset))
         if test_mode:
-            train = get_subset(train)  # take smaller subset of training data to speed up evaluation
+            train = get_subset(train)  # take smaller subset of training data to speed up training
 
-        with TemporaryDirectory() as tmpdir:  # a new temp dir per dataset
-            tmp_path = Path(tmpdir)
+        to_operate_on = {"no_transform": {'train': train,
+                                          'test': test}}
 
-            # write the files for the non-transformed data
-            # we apply a scope to the paths in order to avoid name duplications (not really needed)
-            no_transform_dir = tmp_path / "no_transform"
-            train_paths, test_paths = write_as_feather(train, test, no_transform_dir)
-            to_operate_on: Dict[str, Any] = {"no_transform": {
-                'train_paths': train_paths, 'test_paths': test_paths, 'tmp_path': no_transform_dir}}
+        for pre_process_method in preprocess_models:
+            new_train, new_test = pre_process_method.run(train, test)
+            to_operate_on[pre_process_method.name] = {
+                'train': DataTuple(x=new_train, s=train.s, y=train.y),
+                'test': DataTuple(x=new_test, s=test.s, y=test.y),
+            }
 
-            for pre_process_method in preprocess_models:
-                # separate directory for each preprocessing model
-                model_dir = subdir_for_model(tmp_path, pre_process_method)
-                new_train, new_test = pre_process_method.run_thread(train_paths, test_paths,
-                                                                    model_dir)
+        columns = ['model']
+        columns += [metric.name for metric in metrics]
+        columns += get_sensitive_combinations(per_sens_metrics, train)
 
-                # construct a new path tuple with the new features
-                new_train_paths = PathTuple(x=new_train, s=train_paths.s, y=train_paths.y)
-                new_test_paths = PathTuple(x=new_test, s=test_paths.s, y=test_paths.y)
+        transform_name: str
+        for transform_name, transform in to_operate_on.items():
+            results = pd.DataFrame(columns=columns)
 
-                to_operate_on[pre_process_method.name] = {'train_paths': new_train_paths,
-                                                          'test_paths': new_test_paths,
-                                                          'tmp_path': model_dir}
+            transformed_train: DataTuple = transform['train']
+            transformed_test: DataTuple = transform['test']
 
-            columns = ['model']
-            columns += [metric.name for metric in metrics]
-            columns += get_sensitive_combinations(per_sens_metrics, train)
+            for model in inprocess_models:
 
-            transform_name: str
-            for transform_name, transform in to_operate_on.items():
-                results = pd.DataFrame(columns=columns)
+                temp_res: Dict[str, Union[str, float]] = {'model': model.name}
 
-                transformed_train: PathTuple = transform['train_paths']
-                transformed_test: PathTuple = transform['test_paths']
-                # separate directory for each transformed data set (this should already exist)
-                transform_dir: Path = transform['tmp_path']
+                predictions: pd.DataFrame
+                predictions = model.run(transformed_train, transformed_test)
 
-                for model in inprocess_models:
-                    # separate directory for each model
-                    model_dir = subdir_for_model(transform_dir, model)
+                temp_res.update(run_metrics(predictions, test, metrics, per_sens_metrics))
 
-                    pred_path: Path = model.run_thread(transformed_train, transformed_test,
-                                                       model_dir)
-                    predictions = load_dataframe(pred_path)
-                    temp_res: Dict[str, Union[str, float]] = {'model': model.name}
-                    temp_res.update(run_metrics(predictions, test, metrics, per_sens_metrics))
+                for postprocess in postprocess_models:
+                    # Post-processing has yet to be defined
+                    # - leaving blank until we have an implementation to work with
+                    pass
 
-                    for postprocess in postprocess_models:
-                        # Post-processing has yet to be defined
-                        # - leaving blank until we have an implementation to work with
-                        pass
-
-                    results = results.append(temp_res, ignore_index=True)
-                outdir = Path('..') / 'results'  # OS-independent way of saying '../results'
-                outdir.mkdir(exist_ok=True)
-                results.to_csv(outdir / f"{dataset.name}_{transform_name}.csv", index=False)
-
-
-def subdir_for_model(parent_dir: Path, model: Algorithm) -> Path:
-    """Create a subdirectory in `parent_dir` based on the name of the given model"""
-    model_dir = parent_dir / model.name.strip().replace(' ', '_')
-    model_dir.mkdir(parents=False, exist_ok=False)
-    return model_dir
+                results = results.append(temp_res, ignore_index=True)
+            outdir = Path('..') / 'results'  # OS-independent way of saying '../results'
+            outdir.mkdir(exist_ok=True)
+            results.to_csv(outdir / f"{dataset.name}_{transform_name}.csv", index=False)
