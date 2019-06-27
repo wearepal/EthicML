2c6f83f76e28bc598f09a4674570bf89c34f66d8
Oliver Thomas <olliethomas86@gmail.com>
2019-06-27 15:46:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='EthicML',
-    version='0.1.0',
+    version='0.1.0-alpha',
     author='O. Thomas',
     author_email="olliethomas86@gmail.com",
     description="A toolkit for understanding and researching algorithmic bias",
