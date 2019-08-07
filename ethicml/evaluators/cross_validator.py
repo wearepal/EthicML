4d58e2c7d80dc731fad32be443d3a24c0d027a5f
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-08-07 18:31:33 +0100

    Add cross validation based on two metrics

--- ethicml/evaluators/cross_validator.py
+++ ethicml/evaluators/cross_validator.py
@@ -1,18 +1,23 @@
-""""
-Cross Validation for any in process (at the moment) Algoruthm"""
+"""Cross Validation for any in process (at the moment) Algorithm"""
 
 
 from collections import defaultdict
 from itertools import product
-from operator import itemgetter
 from statistics import mean
-from typing import Dict, List, Tuple, Any
-from typing import Type
+from typing import Dict, List, Tuple, Any, Type, NamedTuple
 
 from ethicml.algorithms.inprocess.in_algorithm import InAlgorithm
 from ethicml.utility.data_structures import DataTuple
 from ethicml.metrics import Accuracy, Metric, CV
 from ethicml.preprocessing.train_test_split import fold_data
+
+
+class ResultTuple(NamedTuple):
+    """Result of one experiment"""
+
+    params: Dict[str, Any]
+    fold_id: int
+    scores: Dict[str, float]
 
 
 class Results:
@@ -21,43 +26,79 @@ class Results:
     """
 
     def __init__(self):
-        self.storage: List[Tuple[Dict[str, Any], int, str, float]] = []
+        self.storage: List[ResultTuple] = []
 
-    def append(self, config: Dict[str, Any], fold_id: int, score_name: str, score: float) -> None:
-        self.storage.append((config, fold_id, score_name, score))
+    def append(self, config: Dict[str, Any], fold_id: int, scores: Dict[str, float]) -> None:
+        self.storage.append(ResultTuple(config, fold_id, scores))
+
+    def _organize_and_compute_means(self) -> Dict[str, ResultTuple]:
+        """Compute means over folds and generate unique string for each hyperparameter setting"""
+        # first, group the entries that have the same hyperparameters
+        max_fold_id = 0
+        grouped: Dict[str, List[ResultTuple]] = defaultdict(list)
+        for result in self.storage:
+            # we convert the hyperparameter dictionaries to strings in order to compare them
+            hyp_string = ", ".join(f"{key!s}={val!r}" for (key, val) in result.params.items())
+            grouped[hyp_string].append(result)
+            if result.fold_id > max_fold_id:
+                max_fold_id = result.fold_id
+
+        # compute the mean value of each measure within each group
+        mean_vals: Dict[str, ResultTuple] = {}
+        for hyp_string, results in grouped.items():
+            # re-order the data: we want a dictionary that has all the scores as a list
+            # first we create empty list according to the dictionary keys in the first result
+            scores_dict: Dict[str, List[float]] = {
+                score_name: [] for score_name in results[0].scores
+            }
+            # then we iterate over results and score names to fill the lists of score values
+            for result in results:
+                for score_name, score in result.scores.items():
+                    scores_dict[score_name].append(score)
+
+            # the re-ordering is complete. now we can compute the mean
+            mean_dict = {score_name: mean(scores) for score_name, scores in scores_dict.items()}
+            # we save everything as a ResultTuple. as we don't have a fold id, we use -1
+            mean_vals[hyp_string] = ResultTuple(results[0].params, -1, mean_dict)
+
+        assert len(mean_vals) * (max_fold_id + 1) == len(self.storage)
+        assert len(list(mean_vals.values())[-1].scores) == len(self.storage[-1].scores)
+        return mean_vals
 
     def get_params_for_best(self, measure: Metric) -> Dict[str, Any]:
         """
         Get the hyperparameter combination for the best performance of a measure
-        Args:
-            measure:
-
-        Returns:
-
         """
-        candidates = [val for val in self.storage if val[2] == measure.name]
+        mean_vals = self._organize_and_compute_means()
 
-        grouped: Dict[str, List[Any]] = defaultdict(list)
-        for tup in candidates:
-            hyp_dic: Dict[str, Any] = tup[0]
-            str_hyp_dict = ", ".join("{!s}={!r}".format(key, val) for (key, val) in hyp_dic.items())
-            grouped[str_hyp_dict].append(tup[1:])
+        def _get_score(item: Tuple[str, ResultTuple]):
+            _, result = item
+            return result.scores[measure.name]
 
-        mean_vals: Dict[str, float] = defaultdict(float)
-        for exp, results in grouped.items():
-            results = [list(res) for res in results]
-            mean_vals[exp] = mean([res[2] for res in results])
+        best_hyp_string, _ = max(mean_vals.items(), key=_get_score)
 
-        best_params = (max(mean_vals.items(), key=itemgetter(1)))[0]
+        return mean_vals[best_hyp_string].params
 
-        params_for_best = candidates[0][0]
-        for k in candidates:
-            hyp_dic = k[0]
-            str_hyp_dict = ", ".join("{!s}={!r}".format(key, val) for (key, val) in hyp_dic.items())
-            if str_hyp_dict == best_params:
-                params_for_best = hyp_dic
-                break
-        return params_for_best
+    def get_params_for_top_k(
+        self, primary: Metric, secondary: Metric, top_k: int
+    ) -> Dict[str, Any]:
+        """
+        First sort the results according to the primary metric, then take the best according to the
+        secondary metric from the top K.
+        """
+        mean_vals = self._organize_and_compute_means()
+
+        def _get_primary_score(item: Tuple[str, ResultTuple]):
+            return item[1].scores[primary.name]
+
+        sorted_by_primary = sorted(mean_vals.items(), key=_get_primary_score)
+        top_k_candidates = sorted_by_primary[:top_k]
+
+        def _get_secondary_score(item: Tuple[str, ResultTuple]):
+            return item[1].scores[secondary.name]
+
+        best_hyp_string, _ = max(top_k_candidates, key=_get_secondary_score)
+        return mean_vals[best_hyp_string].params
 
 
 class CrossValidator:
@@ -91,8 +132,8 @@ class CrossValidator:
             for experiment in self.experiments:
                 model = self.model(**experiment)
                 preds = model.run(train_fold, val)
-                for measure in measures:
-                    self.results.append(experiment, i, measure.name, measure.score(preds, val))
+                scores = {measure.name: measure.score(preds, val) for measure in measures}
+                self.results.append(experiment, i, scores)
                 print(f"fold_{i}_model_{model.name}_completed")
 
     def best(self, measure) -> InAlgorithm:
