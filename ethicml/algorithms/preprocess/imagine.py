cf210e104eff920173f5bc8e2abd2cfd6dfcb262
Oliver Thomas <ot44@sussex.ac.uk>
2019-08-12 12:42:51 +0100

    wip

---
+++ ethicml/algorithms/preprocess/imagine.py
@@ -0,0 +1,53 @@
+from typing import List, Sequence, Tuple
+
+from ethicml.algorithms.preprocess import PreAlgorithmAsync
+from ethicml.algorithms.preprocess.interface import flag_interface
+from ethicml.utility import PathTuple, TestPathTuple, DataTuple, TestTuple
+
+
+class Imagine(PreAlgorithmAsync):
+
+    def __init__(
+        self,
+        enc_size: Sequence[int] = (40,),
+        adv_size: Sequence[int] = (40,),
+        pred_size: Sequence[int] = (40,),
+        batch_size: int = 64,
+        epochs: int = 50,
+        adv_weight: float = 1.0,
+        validation_pcnt: float = 0.1,
+    ):
+        # pylint: disable=too-many-arguments
+        super().__init__()
+        self.enc_size = enc_size
+        self.adv_size = adv_size
+        self.pred_size = pred_size
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.adv_weight = adv_weight
+        self.validation_pcnt = validation_pcnt
+
+    def run(self, train: DataTuple, test: TestTuple) -> Tuple[DataTuple, TestTuple]:
+        from ...implementations import imagine  # only import this on demand because of pytorch
+
+        # SUGGESTION: it would be great if BeutelSettings could already be created in the init
+        flags = imagine.ImagineSettings(
+            enc_size=self.enc_size,
+            adv_size=self.adv_size,
+            pred_size=self.pred_size,
+            batch_size=self.batch_size,
+            epochs=self.epochs,
+            adv_weight=self.adv_weight,
+            validation_pcnt=self.validation_pcnt,
+        )
+        return imagine.train_and_transform(train, test, flags)
+
+    def _script_command(self, train_paths: PathTuple, test_paths: TestPathTuple,
+                        new_train_paths: PathTuple, new_test_paths: TestPathTuple) -> List[str]:
+        args = flag_interface(train_paths, test_paths, new_train_paths, new_test_paths, vars(self))
+        return ["-m", "ethicml.implementations.imagine"] + args
+
+
+@property
+def name(self) -> str:
+    return "Imagined Examples"
