125f5b117ecadffc266adf7511994aff85a3417c
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-08-18 20:53:33 +0100

    Avoid (unsafe) casting of pd.Series to int
    This can fail at run-time if there is more than one element in the
    Series object.

--- ethicml/preprocessing/feature_binning.py
+++ ethicml/preprocessing/feature_binning.py
@@ -11,20 +11,16 @@ from ethicml.utility import DataTuple
 def bin_cont_feats(data: DataTuple) -> DataTuple:
     """
     Given a datatuple, bin the columns that have ordinal features and return as afresh new DataTuple
-    Args:
-        data:
-
-    Returns:
-
     """
     groups: List[List[str]] = [
-        list(group) for key, group in groupby(data.x.columns, lambda x: x.split('_')[0])
+        list(group) for _, group in groupby(data.x.columns, lambda x: x.split('_')[0])
     ]
 
     copy: pd.DataFrame = data.x.copy()
 
     for group in groups:
-        if len(group) == 1 and int(data.x[group].nunique()) > 2:
+        # if there is only one element in the group, then it corresponds to a continuous feature
+        if len(group) == 1 and data.x[group[0]].nunique() > 2:
             copy[group] = pd.cut(data.x[group].to_numpy()[:, 0], 5)
             copy = pd.concat([copy, pd.get_dummies(copy[group])], axis="columns")
             copy = copy.drop(group, axis=1)
