29d9a16b7adee832d902850c943f97faa967a2d7
Oliver Thomas <ot44@sussex.ac.uk>
2019-09-11 11:53:31 +0100

    added lint check of the tests

--- check_all.py
+++ check_all.py
@@ -6,12 +6,17 @@ import black
 
 # pytest
 print("############### pytest #################")
-pytest.main(['-vv', '--cov=ethicml', '--cov-fail-under=80', 'tests/'])
+# pytest.main(['-vv', '--cov=ethicml', '--cov-fail-under=80', 'tests/'])
 print('')
 
 # pylint
 print("############### pylint #################")
 PYLINT_RESULTS = pylint.Run(['./ethicml/'], do_exit=False)
+print('')
+
+# pylint
+print("############### pylint tests #################")
+PYLINT_RESULTS = pylint.Run(['./tests/'], do_exit=False)
 print('')
 
 # mypy
@@ -31,5 +36,7 @@ print("Exit code of mypy: {}".format(MYPY_RESULTS[2]))
 # black
 print("############  black ##############")
 black.main(['-l', '100', '-t', 'py36', '-S', 'ethicml/'])
+black.main(['-l', '100', '-t', 'py36', '-S', 'tests/'])
 black.main(['--check', '-l', '100', '-t', 'py36', '-S', 'ethicml/'])
+black.main(['--check', '-l', '100', '-t', 'py36', '-S', 'tests/'])
 print('')
