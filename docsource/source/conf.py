fd217bdf1a0c3c6ad46359f33608333625513ed8
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-09-17 17:03:39 +0100

    Change syntax highlighting style

--- docsource/source/conf.py
+++ docsource/source/conf.py
@@ -68,6 +68,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'  # 'alabaster'
+pygments_style = 'sphinx'  # syntax highlighting style to use
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
