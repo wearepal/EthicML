fdec566b73cd7bc5f85850d708e24a08a11e95d7
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-10-20 15:16:54 +0100

    Add F1 score to metrics

--- ethicml/metrics/accuracy.py
+++ ethicml/metrics/accuracy.py
@@ -1,21 +1,48 @@
-"""
-For assessing accuracy
-"""
+"""Accuracy and related metrics"""
+from typing import Callable, Optional
 
 import pandas as pd
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 
 from ethicml.utility.data_structures import DataTuple
 from .metric import Metric
 
+__all__ = ["Accuracy", "F1", "SklearnMetric"]
 
-class Accuracy(Metric):
-    """Computes the classification accuracy"""
+
+class SklearnMetric(Metric):
+    """Wrapper around an sklearn metric"""
+
+    def __init__(
+        self,
+        sklearn_metric: Callable[[pd.DataFrame, pd.DataFrame], float],
+        name: str,
+        pos_class: Optional[int] = None,
+    ):
+        if pos_class is not None:
+            super().__init__(pos_class=pos_class)
+        else:
+            super().__init__()
+        self._metric = sklearn_metric
+        self._name = name
 
     def score(self, prediction: pd.DataFrame, actual: DataTuple) -> float:
-        actual_y = actual.y.to_numpy()
-        return accuracy_score(actual_y, prediction)
+        return self._metric(actual.y, prediction)
 
     @property
     def name(self) -> str:
-        return "Accuracy"
+        return self._name
+
+
+class Accuracy(SklearnMetric):
+    """Classification accuracy"""
+
+    def __init__(self, pos_class: Optional[int] = None):
+        super().__init__(accuracy_score, "Accuracy", pos_class=pos_class)
+
+
+class F1(SklearnMetric):
+    """F1 score: harmonic mean of precision and recall"""
+
+    def __init__(self, pos_class: Optional[int] = None):
+        super().__init__(f1_score, "F1", pos_class=pos_class)
