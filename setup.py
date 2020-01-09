1974c5997d60cfb74b13c9e4d6b3d21d9dc70cab
thomkeh <7741417+thomkeh@users.noreply.github.com>
2020-01-09 19:55:13 +0000

    Downgrade pillow

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         "numpy >= 1.14.2",
         "pandas >= 0.24.0",
         "pipenv >= 2018.11.26",
-        "pillow",
+        "pillow < 7.0",
         "pyarrow >= 0.11",
         "scikit_learn >= 0.20.1",
         "seaborn >= 0.9.0",
