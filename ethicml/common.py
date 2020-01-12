cfe422f6ce993fc2e733e2f41b01a06b21307dca
thomkeh <7741417+thomkeh@users.noreply.github.com>
2020-01-12 16:48:39 +0000

    Rename some variables in ethicml/common.py

--- ethicml/common.py
+++ ethicml/common.py
@@ -2,26 +2,25 @@
 
 import os
 from pathlib import Path
-from typing import Callable, Any, TypeVar, Type
+from typing import Any, Callable, Type, TypeVar
 
 ROOT_DIR: str = os.path.abspath(os.path.join(os.path.abspath(__file__), os.pardir))
 ROOT_PATH: Path = Path(__file__).parent.resolve()
 
-FuncType = Callable[..., Any]
-_F = TypeVar('_F', bound=FuncType)
+_F = TypeVar('_F', bound=Callable[..., Any])
 
 
 class implements:  # pylint: disable=invalid-name
     """Mark a function as implementing an interface."""
 
-    def __init__(self, class_: Type):
-        """Instantiate this decorator.
+    def __init__(self, interface: Type):
+        """Instantiate the decorator.
 
         Args:
-            class_: the interface which we're implementing
+            interface: the interface that is implemented
         """
-        self.class_ = class_
+        self.interface = interface
 
     def __call__(self, func: _F) -> _F:
-        """Identity function."""
+        """Take a function and return it unchanged."""
         return func
