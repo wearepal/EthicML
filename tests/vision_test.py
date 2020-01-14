7837a0d75efd66e0fcf1654ffffc9e5822e9e613
mb715 <mb715@sussex.ac.uk>
2020-01-14 18:33:21 +0000

    Format with black.

--- tests/vision_test.py
+++ tests/vision_test.py
@@ -24,5 +24,5 @@ def test_label_dependent_transforms(transform):
         greyscale=False,
     )
 
-TODO: make better assertions
-    augmented_data = colorizer(data, labels)
+    #   TODO: test with meaningful assertions
+    colorizer(data, labels)
