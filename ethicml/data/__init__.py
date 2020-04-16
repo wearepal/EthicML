a305a627e5d4001e836eedb61b7421254a87ee8a
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2020-04-16 01:01:05 +0100

    pre-commit gets downgraded for some reason

--- ethicml/data/__init__.py
+++ ethicml/data/__init__.py
@@ -1,1 +1,15 @@
 """This module contains items related to data, such as raw csv's and data objects."""
+from .load import *
+from .tabular_data.adult import *
+from .tabular_data.compas import Compas
+from .tabular_data.configurable_dataset import *
+from .tabular_data.credit import *
+from .tabular_data.dataset import *
+from .tabular_data.german import *
+from .tabular_data.non_binary_toy import *
+from .tabular_data.sqf import *
+from .tabular_data.toy import *
+from .tabular_data.util import *
+from .vision_data.celeba import *
+from .vision_data.genfaces import *
+from .vision_data.image_dataset import *
