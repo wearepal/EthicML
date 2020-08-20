b6d7cba598216712757788cf29cf1f7bd3c668c7
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2020-08-20 17:49:46 +0100

    blaaaack

--- ethicml/data/csvs/utility.py
+++ ethicml/data/csvs/utility.py
@@ -5,4 +5,4 @@ import numpy as np
 
 def sigmoid(x: np.ndarray) -> np.ndarray:
     """As there is no np.sigmoid."""
-    return np.where(x >= 0,  1 / (1 + np.exp(-x)),  np.exp(x) / (np.exp(x) + 1))
+    return np.where(x >= 0, 1 / (1 + np.exp(-x)), np.exp(x) / (np.exp(x) + 1))
