09aaa32645bd89fb3946c12075331d5f1d18f58a
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2020-08-26 18:05:15 +0100

    blaaaaack

--- ethicml/visualisation/plot.py
+++ ethicml/visualisation/plot.py
@@ -105,9 +105,13 @@ def multivariateGrid(
     legends = []
     for name, df_group in df.groupby([sens_col, outcome_col]):
         legends.append(f"S={name[0]}, Y={name[1]}")
-        g.plot_joint(colored_scatter(df_group[col_x], df_group[col_y], color),)
+        g.plot_joint(
+            colored_scatter(df_group[col_x], df_group[col_y], color),
+        )
         sns.distplot(
-            df_group[col_x].values, ax=g.ax_marg_x, color=color,
+            df_group[col_x].values,
+            ax=g.ax_marg_x,
+            color=color,
         )
         sns.distplot(df_group[col_y].values, ax=g.ax_marg_y, vertical=True)
     # Do also global Hist:
