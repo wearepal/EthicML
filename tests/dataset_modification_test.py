81b55a6843c15582c5d53c07782ea9ada4ba4915
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2021-03-13 11:16:22 +0000

    add flag to scale continuous to just do transform only

--- tests/dataset_modification_test.py
+++ tests/dataset_modification_test.py
@@ -30,3 +30,37 @@ def test_scaling(dataset_name, scaler):
     datatuple_post, _ = scale_continuous(dataset, datatuple_scaled, scaler2, inverse=True)
 
     pandas.testing.assert_frame_equal(datatuple.x, datatuple_post.x, check_dtype=False)
+
+
+@pytest.mark.parametrize("dataset_name", available_tabular())
+@pytest.mark.parametrize("scaler", [StandardScaler, MinMaxScaler])
+def test_scaling(dataset_name, scaler):
+    """Test that scaling works."""
+    scaler = scaler()
+    dataset = get_dataset_obj_by_name(dataset_name)()
+    datatuple = dataset.load()
+
+    # Speed up the tests by making some data smaller
+    if dataset_name == "health":
+        datatuple, _ = train_test_split(datatuple, train_percentage=0.05)
+
+    train, test = train_test_split(datatuple)
+
+    train_scaled, scaler2 = scale_continuous(dataset, train, scaler)
+    test_scaled, _ = scale_continuous(dataset, test, scaler2, fit=False)
+
+    if dataset_name == "crime" and str(scaler) == "MinMaxScaler()":
+        # Crime dataset is minmax scaled by the data providers.
+        # So can't confirm that train contains the full range
+        pass
+    else:
+        with pytest.raises(AssertionError):
+            pandas.testing.assert_frame_equal(train.x, train_scaled.x, check_dtype=False)
+        with pytest.raises(AssertionError):
+            pandas.testing.assert_frame_equal(test.x, test_scaled.x, check_dtype=False)
+
+    train_post, _ = scale_continuous(dataset, train_scaled, scaler2, inverse=True)
+    test_post, _ = scale_continuous(dataset, test_scaled, scaler2, inverse=True)
+
+    pandas.testing.assert_frame_equal(train.x, train_post.x, check_dtype=False)
+    pandas.testing.assert_frame_equal(test.x, test_post.x, check_dtype=False)
