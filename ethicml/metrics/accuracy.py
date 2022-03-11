19ce1b2207cd2940cc658807def5c9c0826a8973
Thomas MK <tmke@posteo.net>
2022-03-11 15:05:17 +0100

    Use BaseMetric also for the SklearnMetrics

--- ethicml/metrics/accuracy.py
+++ ethicml/metrics/accuracy.py
@@ -1,6 +1,6 @@
 """Accuracy and related metrics."""
 from dataclasses import dataclass
-from typing import Callable, Tuple
+from typing import Callable, ClassVar, Tuple
 from typing_extensions import Protocol
 
 import pandas as pd
@@ -9,18 +9,18 @@ from sklearn.metrics import accuracy_score, f1_score
 
 from ethicml.utility import DataTuple, Prediction
 
-from .metric import Metric
+from .metric import BaseMetric
 
 __all__ = ["Accuracy", "F1", "SklearnMetric"]
 
 
-class SklearnMetric(Metric, Protocol):
+class SklearnMetric(BaseMetric, Protocol):
     """Wrapper around an sklearn metric."""
 
     # we have to store the callable in a 1-element tuple because otherwise mypy gets confused
-    sklearn_metric: Tuple[Callable[[pd.DataFrame, pd.Series], float]]
+    sklearn_metric: ClassVar[Tuple[Callable[[pd.DataFrame, pd.Series], float]]]
 
-    @implements(Metric)
+    @implements(BaseMetric)
     def score(self, prediction: Prediction, actual: DataTuple) -> float:
         return self.sklearn_metric[0](actual.y, prediction.hard)
 
@@ -30,12 +30,8 @@ class Accuracy(SklearnMetric):
     """Classification accuracy."""
 
     sklearn_metric = (accuracy_score,)
-    apply_per_sensitive = True
-
-    @property
-    def name(self) -> str:
-        """Name of the metric."""
-        return "Accuracy"
+    apply_per_sensitive: ClassVar[bool] = True
+    _name: ClassVar[str] = "Accuracy"
 
 
 @dataclass
@@ -43,9 +39,5 @@ class F1(SklearnMetric):
     """F1 score: harmonic mean of precision and recall."""
 
     sklearn_metric = (f1_score,)
-    apply_per_sensitive = True
-
-    @property
-    def name(self) -> str:
-        """Name of the metric."""
-        return "F1"
+    apply_per_sensitive: ClassVar[bool] = True
+    _name: ClassVar[str] = "F1"
