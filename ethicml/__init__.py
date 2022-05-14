2630337b53de522ca767e5a79d73f4a637c718af
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2022-05-15 00:23:17 +0100

    add to init

--- ethicml/__init__.py
+++ ethicml/__init__.py
@@ -2,6 +2,7 @@
 from .algorithms.algorithm_base import *
 from .algorithms.inprocess.agarwal_reductions import *
 from .algorithms.inprocess.blind import *
+from .algorithms.inprocess.fair_dummies import *
 from .algorithms.inprocess.fairness_wo_demographics import *
 from .algorithms.inprocess.in_algorithm import *
 from .algorithms.inprocess.in_subprocess import *
